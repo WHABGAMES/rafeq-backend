a5efbacc25e454726a51e63ec358a8667b0ea38a
WHABGAMES <52535549+WHABGAMES@users.noreply.github.com>
2026-02-07 13:52:22 +0400

    Update encryption.util.ts

--- src/common/utils/encryption.util.ts
+++ src/common/utils/encryption.util.ts
@@ -17,7 +17,6 @@ import * as crypto from 'crypto';
 const ALGORITHM = 'aes-256-gcm';
 const IV_LENGTH = 16;       // 16 bytes = 128 bits
 const AUTH_TAG_LENGTH = 16; // 16 bytes = 128 bits
-const KEY_LENGTH = 32;      // 32 bytes = 256 bits
 
 /**
  * جلب مفتاح التشفير من البيئة
