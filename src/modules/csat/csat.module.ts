c6ca64c29fcc9dd9edc6bd1a745aa80e0838d170
WHABGAMES <52535549+WHABGAMES@users.noreply.github.com>
2026-02-24 12:02:45 +0400

    Update csat.module.ts

--- src/modules/csat/csat.module.ts
+++ src/modules/csat/csat.module.ts
@@ -5,10 +5,15 @@
  */
 
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CsatController } from './csat.controller';
 import { CsatService } from './csat.service';
+import { CsatSurvey } from './entities/csat-survey.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([CsatSurvey]),
+  ],
   controllers: [CsatController],
   providers: [CsatService],
   exports: [CsatService],
