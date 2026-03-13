9bd26346f1354ca5ed557c31043d8d03f90c661a
WHABGAMES <52535549+WHABGAMES@users.noreply.github.com>
2026-03-13 19:00:50 +0400

    Create index.ts

---
+++ src/modules/suggestions/entities/index.ts
@@ -0,0 +1,4 @@
+export { Suggestion, SuggestionType, SuggestionStatus } from './suggestion.entity';
+export { SuggestionLike } from './suggestion-like.entity';
+export { SuggestionComment } from './suggestion-comment.entity';
+export { SuggestionFollower } from './suggestion-follower.entity';
