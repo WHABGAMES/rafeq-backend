3ca00156db475d307262b0fe056f9fd2ab8c1a1a
WHABGAMES <52535549+WHABGAMES@users.noreply.github.com>
2026-04-02 23:22:57 +0400

    Update otp-relay.module.ts

--- src/modules/otp-relay/otp-relay.module.ts
+++ src/modules/otp-relay/otp-relay.module.ts
@@ -2,14 +2,17 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { OtpConfig, OtpRequestLog } from './entities/otp-config.entity';
 import { Store } from '../stores/entities/store.entity';
+import { Channel } from '../channels/entities/channel.entity';
 import { OtpRelayService } from './otp-relay.service';
 import { OtpRelayController, OtpPublicController } from './otp-relay.controller';
 import { StoresModule } from '../stores/stores.module';
+import { ChannelsModule } from '../channels/channels.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([OtpConfig, OtpRequestLog, Store]),
+    TypeOrmModule.forFeature([OtpConfig, OtpRequestLog, Store, Channel]),
     StoresModule,
+    ChannelsModule,
   ],
   controllers: [OtpRelayController, OtpPublicController],
   providers: [OtpRelayService],
