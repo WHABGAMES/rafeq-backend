0e6e875afc9574a26b06714b50b1eb77d508d17b
WHABGAMES <52535549+WHABGAMES@users.noreply.github.com>
2026-04-07 11:15:21 +0400

    Update otp-relay.module.ts

--- src/modules/otp-relay/otp-relay.module.ts
+++ src/modules/otp-relay/otp-relay.module.ts
@@ -1,21 +1,23 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { OtpConfig, OtpRequestLog } from './entities/otp-config.entity';
+import { OtpInventoryItem, OtpCompensation } from './entities/otp-inventory.entity';
 import { Store } from '../stores/entities/store.entity';
 import { Channel } from '../channels/entities/channel.entity';
 import { OtpRelayService } from './otp-relay.service';
+import { OtpInventoryService } from './otp-inventory.service';
 import { OtpRelayController, OtpPublicController } from './otp-relay.controller';
 import { StoresModule } from '../stores/stores.module';
 import { ChannelsModule } from '../channels/channels.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([OtpConfig, OtpRequestLog, Store, Channel]),
+    TypeOrmModule.forFeature([OtpConfig, OtpRequestLog, OtpInventoryItem, OtpCompensation, Store, Channel]),
     StoresModule,
     ChannelsModule,
   ],
   controllers: [OtpRelayController, OtpPublicController],
-  providers: [OtpRelayService],
-  exports: [OtpRelayService],
+  providers: [OtpRelayService, OtpInventoryService],
+  exports: [OtpRelayService, OtpInventoryService],
 })
 export class OtpRelayModule {}
